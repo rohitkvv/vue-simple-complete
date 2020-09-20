4da95a4ee23611630aef3d4223de68cd5793582a
rohitkvv <rohitkvv@gmail.com>
2020-09-20 16:51:15 +0200

    first publish

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import SimpleComplete from "./SimpleComplete.vue";
+
+const SimpleCompleteComponent = {
+  SimpleComplete
+};
+
+Vue.component("vue-simple-complete", SimpleComplete);
+
+export default SimpleCompleteComponent;
